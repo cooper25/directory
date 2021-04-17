9097ed27250c91b669950f4167ee392c5a675442
cooper25 <me@coopertsoris.dev>
2021-04-17 18:18:42 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  siteMetadata: {
+    title: "directory-react",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-offline",
+  ],
+};
