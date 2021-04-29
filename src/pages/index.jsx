94b07b455c3c61954ec274b341303be028a19987
Cooper <me@coopertsoris.dev>
2021-04-29 11:40:36 -0500

    📝 Remove Header

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,7 +23,6 @@ const IndexPage = () => {
         </div>
       </div>
       <div className="right_image-container">
-         <header><b>Did you know my directory is open source?</b> You can view it on Github by clicking <a target="_blank" rel="noopener noreferrer" href="https://github.com/cooper25/directory/">here</a></header>
         <div className="right_image"></div>
         <footer>Photo by <a target="_blank" rel="noopener noreferrer" href="https://unsplash.com/@linglivestolaugh">Ling Tang</a> on <a target="_blank" rel="noopener noreferrer" href="https://unsplash.com/t/wallpapers">Unsplash</a>   </footer>
       </div>
