99f998db2dfe207463e1bb2ade3a8a4effa47c43
Cooper <me@coopertsoris.dev>
2021-05-06 11:44:20 -0500

    Update index.jsx

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -16,14 +16,14 @@ const IndexPage = () => {
         <meta charset="UTF-8"/>
         <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "cc640b32a0bb4966b346d0f9083e3489"}'></script>
         <script>
-            (function(h,o,t,j,a,r){
+            {(function(h,o,t,j,a,r){
                 h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
                 h._hjSettings={hjid:2387411,hjsv:6};
                 a=o.getElementsByTagName('head')[0];
                 r=o.createElement('script');r.async=1;
                 r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
                 a.appendChild(r);
-            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
+            })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=')}
         </script>
       </Helmet>
       <div className="cursor"></div>
