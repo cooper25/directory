eb3c46bac10bebf65a424fe1b8ccdd14d4bda5f1
cooper25 <me@coopertsoris.dev>
2021-05-13 21:38:54 -0500

    ✨ Update Font & Minor Tweaks

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -8,20 +8,22 @@ const IndexPage = () => {
   return (
     <body lang="en">
       <Helmet>
+        <meta charset="UTF-8"/>
         <title>cppr.design - directory</title>
         <link rel="shortcut icon" type="image/png" href={favicon}></link>
         <meta name="author" content="Cooper Tsoris"/>
         <meta name="theme-color" content="#000000"/>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta charset="UTF-8"/>
         <script defer src='https://static.cloudflareinsights.com/beacon.min.js' data-cf-beacon='{"token": "cc640b32a0bb4966b346d0f9083e3489"}'></script>
         <script src='https://cooper25.github.io/hotjar.js'></script>
+        <link rel="stylesheet" href="https://use.typekit.net/opj2khw.css"></link>
+        <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=inter:wght@100;200;300;400;500;600;700;800;900&display=swap"></link>
       </Helmet>
       <div className="cursor"></div>
       <div className="elements">
         <img src={name} draggable="false" alt="cooper"/><br/><br/>
         <div className="buttons">
-          <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://discord.com/users/307273393600724994/"><button>Discord</button></a>
+          <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://discord.com/users/307273393600724994/"><button id="main">cooper#0325</button></a>
           <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://twitter.com/loertis_"><button>Twitter</button></a>
           <a draggable="false" target="_blank" rel="noopener noreferrer" href="https://behance.net/coopertsoris"><button>Behance</button></a>
           <a draggable="false" target="_blank" rel="noopener noreferrer" href="mailto:cooper@cppr.dev"><button>Email</button></a>
